ef33ab8dc803cdabcf3549216f7be2b804184149
AnettKeszler <anett.dr.keszler@gmail.com>
2021-03-01 00:12:52 +0100

    add OAuth to pom.xml, update application.properties with github ClientID and refactor controller

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -18,13 +15,15 @@ public class HomeController {
     private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
 
     @GetMapping("/")
-    public String getCurrentUser(@AuthenticationPrincipal User user, Model model) {
-        String username = user.getUsername();
+    public String getCurrentUser(@AuthenticationPrincipal OidcUser user, Model model) {
+        String email = user.getEmail();
 
-        model.addAttribute("username", username);
-        model.addAttribute("lastAccess", usersLastAccess.get(username));
+        model.addAttribute("email", email);
+        model.addAttribute("lastAccess", usersLastAccess.get(email));
+        model.addAttribute("firstName", user.getGivenName());
+        model.addAttribute("lastName", user.getFamilyName());
 
-        usersLastAccess.put(username, LocalDateTime.now());
+        usersLastAccess.put(email, LocalDateTime.now());
 
         return "home";
 
