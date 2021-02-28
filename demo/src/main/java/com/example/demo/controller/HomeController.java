6f882202144df4ba61ff4574e21f07a76d2bc0da
AnettKeszler <anett.dr.keszler@gmail.com>
2021-02-28 23:34:28 +0100

    create controller, home page and security file

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+public class HomeController {
+    private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
+
+    @GetMapping("/")
+    public String getCurrentUser(@AuthenticationPrincipal User user, Model model) {
+        String username = user.getUsername();
+
+        model.addAttribute("username", username);
+        model.addAttribute("lastAccess", usersLastAccess.get(username));
+
+        usersLastAccess.put(username, LocalDateTime.now());
+
+        return "home";
+
+
+    }
+
+
+
+}
