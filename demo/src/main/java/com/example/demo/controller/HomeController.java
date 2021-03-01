b689606629d29a50d2ed9a8e7d4fe77220194b31
AnettKeszler <anett.dr.keszler@gmail.com>
2021-03-01 09:46:28 +0100

    refactor to be authorized with GitHub

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,15 +17,17 @@ public class HomeController {
     private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
 
     @GetMapping("/")
-    public String getCurrentUser(@AuthenticationPrincipal OidcUser user, Model model) {
-        String email = user.getEmail();
+    public String getCurrentUser(@AuthenticationPrincipal OAuth2User user, Model model) {
 
-        model.addAttribute("email", email);
-        model.addAttribute("lastAccess", usersLastAccess.get(email));
-        model.addAttribute("firstName", user.getGivenName());
-        model.addAttribute("lastName", user.getFamilyName());
+        Collections.singletonMap("name", user.getAttribute("name"));
 
-        usersLastAccess.put(email, LocalDateTime.now());
+//        String email = user.getEmail();
+//
+//        model.addAttribute("email", email);
+//        model.addAttribute("lastAccess", usersLastAccess.get(email));
+//        model.addAttribute("firstName", user.getGivenName());
+//        model.addAttribute("lastName", user.getFamilyName());
+//        usersLastAccess.put(email, LocalDateTime.now());
 
         return "home";
 
